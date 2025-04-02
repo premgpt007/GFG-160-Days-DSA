d3a4535dd0f9f4ab26afb4c6d1268ec2d9b8d5cd
Prem Gupta <140595984+premgpt007@users.noreply.github.com>
2025-04-02 09:36:10 +0530

    2nd Largest Integer.java

--- 2nd Largest Integer.java
+++ 2nd Largest Integer.java
@@ -28,12 +28,12 @@ class Solution {
     public int getSecondLargest(int[] arr) {
         int first = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
-        for(int num : arr){
-            if(num > first){
+        for (int num : arr){
+            if (num > first){
                 second = first;
                 first = num;
             }
-            else if(num > second && num != first){
+            else if (num > second && num != first){
                 second = num;
             }
         }
